b2e43a407246673433f6f12ab7c387ddb54627a7
Carlos Leon <carlosleon@Carloss-MacBook-Air.local>
2022-05-20 15:15:30 -0500

    Adding validation with Joi part01

---
+++ schemas/product.schema.js
@@ -0,0 +1,10 @@
+const Joi = require("joi");
+
+const id = Joi.string().uuid();
+const name = Joi.string().alphanum().min(3).max(15);
+const price = Joi.number().integer().min(10);
+
+const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required()
+});
