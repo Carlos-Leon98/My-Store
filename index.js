989c010c08d5c58f1451929b542d94f2d911f379
Carlos Leon <carlosleon@Carloss-Air.lan1>
2022-05-21 09:33:34 -0500

    Added the Procfile

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const routerApi = require("./routes");
 const { logErrors, errorHandler, boomErrorHandler } = require("./middlewares/error.handler");
 
 const app =  express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const whiteList = ["http://localhost:8080", "https://myapp.co"];
 const options = {
   origin: (origin, callback) => {
-    if (whiteList.includes(origin)) {
+    if (whiteList.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not Allow"));
